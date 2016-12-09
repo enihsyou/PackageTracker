6e2da356fc5e9bbee3c25ca33846bd83e5106527
九条涼果 <s1131234@gmail.com>
2016-12-09 19:37:05 +0800

    获取快递公司代码的爬虫模块

---
+++ scraper/kuaidi100.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+import requests
+from bs4 import BeautifulSoup, Tag
+
+url = r"https://www.kuaidi100.com/"
+
+
+def get_html(url):
+    response = requests.get(url=url)
+    return response.text
+
+
+def get_soup(response_body):
+    return BeautifulSoup(response_body, "lxml")
+
+
+def get_links(soup: Tag):
+    lists = soup.find_all("div", class_="com-list")
+    par = {}
+    for each_list in lists:  # type: Tag
+        for item in each_list.find_all("a"):
+            if not item.has_attr("data-code"): continue
+            if item["data-code"] == "default": continue
+
+            code = item["data-code"]
+            code_text = item.text
+
+            par[code] = code_text
+            print(code, code_text)
+
+    return par
+
+
+if __name__ == "__main__":
+    par = get_links(get_soup(get_html(url)))
+    with open("code.txt", "w") as file:
+        for key, value in par.items():
+            file.write("{} {}\n".format(key, value))
